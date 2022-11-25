2f7318b5d4676e08bf3eb1afdfd681c8313a02af
sergey.osv <opc-66@yandex.ru>
2022-11-25 17:16:40 +0300

    Sem 2 HW 1

---
+++ Seminar_2/Sevmnar2_HW1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, кот принимает на вх
+//  трехзначное число и на вых показывает вторую цифру эт числа
+
+int num = new Random().Next(100,1000);
+
+Console.WriteLine(num);
+
+int res = (num/10)%10;
+
+Console.WriteLine(res);
+
