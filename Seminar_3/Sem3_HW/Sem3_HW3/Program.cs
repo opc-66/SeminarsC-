a4b8e89b654b3a9c375029f7bdacf96ed1665150
sergey.osv <opc-66@yandex.ru>
2022-11-26 13:44:48 +0300

    Sem 3 HW\ HW3

---
+++ Seminar_3/Sem3_HW/Sem3_HW3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23
+// Напишите программу, которая принимает 
+// на вход число (N) и выдаёт таблицу кубов
+//  чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("ВВедите  N ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (count<= N)
+{
+    Console.WriteLine(Math.Pow(count,3));
+    count=count+1;
+}
+
