09e91a897936e42b3538edc55e0433f542f718fc
sergey.osv <opc-66@yandex.ru>
2022-11-26 16:20:54 +0300

    Sem 4 _2

---
+++ Seminar_4/Seminar_4_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 26: Напишите программу, которая
+//  принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int GetCounNum(int res)
+{
+
+int count =0;
+
+while(res>0)
+{
+count++;  //
+res/=10;   //res=res/10
+
+}
+return count;
+
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Колличество цифр "+GetCounNum(num));
