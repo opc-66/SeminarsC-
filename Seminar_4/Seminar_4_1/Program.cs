f5de0004ca625a70e62e6006b819554f4f5250dc
sergey.osv <opc-66@yandex.ru>
2022-11-26 14:59:45 +0300

    Sem 4\ Sem 4_1

--- Seminar_4/Seminar_4_1/Program.cs
+++ Seminar_4/Seminar_4_1/Program.cs
@@ -1,5 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая
+//  принимает на вход число (А) и выдаёт 
+//  сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
 
 
-fdjdhg
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(GetSumNum(num));
+
+int GetSumNum(int res)
+{
+int sum =0;
+int count =0;
+
+while(count<res)
+{
+count=count+1;    //count++
+sum = sum+count;  // sum +=count;
+
+}
+return sum;
+
+}
