bcc164e44f44610b137173ae1cab1529c188b57d
sergey.osv <opc-66@yandex.ru>
2022-11-26 17:52:17 +0300

    Sem 4 HW\ HW1

--- Seminar_4/Sem4_HW/Sem4_HW1/Program.cs
+++ Seminar_4/Sem4_HW/Sem4_HW1/Program.cs
@@ -4,3 +4,21 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+Console.WriteLine("Введите число 1 ");
+double A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2 ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+double Pow(double num1, int num2)
+{
+    double result = 1;
+    for (int i=0; i <num2; i++)  //i=0 если i<num2 тогда
+    {
+        result *= num1;           // result=result*num1 ->//i=i+1
+    }
+    return result;                               
+}
+
+double res = Pow(A, B);
+Console.WriteLine(res);
