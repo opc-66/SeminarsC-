6e1ad575d7257610bcadb48d87bcb96b80d39d9f
sergey.osv <opc-66@yandex.ru>
2022-12-01 13:07:58 +0300

    Sem 5 HW 2

--- Seminar_5/Sem5_HW/Sem5_HW2/Program.cs
+++ Seminar_5/Sem5_HW/Sem5_HW2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 36: Задайте одномерный массив, заполненный
+//  случайными числами. Найдите сумму элементов, 
+//  стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите длину массива ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+
+int [] array =  GetArray(n);
+int [] GetArray(int num)
+{
+    int [] array = new int[num];
+
+for (int i = 0; i <num; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+        if (i%2 !=0)
+        {
+            res = res+ array[i];
+        }
+    }
+    return array;
+}
+Console.Write(String.Join(" ", array));
+Console.WriteLine();
+Console.WriteLine("СУмма чисел на нечетных позициях " +res);
