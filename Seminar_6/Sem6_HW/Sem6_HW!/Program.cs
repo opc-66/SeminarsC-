e3ff256145786834c9ce4a4eba661a837a521d0e
sergey.osv <opc-66@yandex.ru>
2022-12-02 15:18:42 +0300

    Sem 7 HW !

--- Seminar_6/Sem6_HW/Sem6_HW!/Program.cs
+++ Seminar_6/Sem6_HW/Sem6_HW!/Program.cs
@@ -1,6 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M
+//  чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
 
+Console.WriteLine("Введите количество элементов массива");
+int num = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [num];
+int count = 0;
+int i = 0;
 
+for(i = 0; i < array.Length;i++)
+{
+    Console.WriteLine($"Введите элемент массива {i}");
+    array[i] = Convert.ToInt32(Console.ReadLine());
 
-shsjs
+     if(array[i]>0)
+    {
+        count = count +1;
+
+    }
+
+}
+// return array;
+
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine($"Количество положительных чисел  {count}");
