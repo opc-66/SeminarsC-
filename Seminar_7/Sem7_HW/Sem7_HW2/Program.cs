b1f8d102325af58410671e90b936a9ea5a347c2d
sergey.osv <opc-66@yandex.ru>
2022-12-03 20:36:16 +0300

    Sem 7 HW 2

--- Seminar_7/Sem7_HW/Sem7_HW2/Program.cs
+++ Seminar_7/Sem7_HW/Sem7_HW2/Program.cs
@@ -1,6 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 50. Напишите программу, которая 
+// на вход принимает позиции элемента в двумерном
+//  массиве, и возвращает значение этого элемента или же
+//  указание, что такого элемента нет.
 
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
 
-gq'gjq
-jg
+Console.WriteLine("Input m");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [m, n];
+
+for (int i=0; i< matrix.GetLength(0); i++)
+{
+    for (int j=0; j<matrix.GetLength(1); j++)
+    {
+        matrix [i, j]= new Random().Next(0, 10);
+        
+        Console.Write(matrix[i,j] +" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите номер строки");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите номер столбца");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a<m&&b<n)
+{
+    Console.WriteLine("Элемент массива " + matrix[a,b]);
+}
+else
+Console.WriteLine("Элемента с таким номером нет");
