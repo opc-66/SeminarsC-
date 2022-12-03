e221d4b4f86acb01f6d200cae90588622218a5a4
sergey.osv <opc-66@yandex.ru>
2022-12-03 19:56:14 +0300

    Sem 7 HW 1

--- Seminar_7/Sem7_HW/Sem7_HW1/Program.cs
+++ Seminar_7/Sem7_HW/Sem7_HW1/Program.cs
@@ -1,5 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив 
+// размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
 
+Console.WriteLine("Input m");
+int m = Convert.ToInt32(Console.ReadLine());
 
-'idhf'qwdh
+Console.WriteLine("Input n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix = new double [m, n];
+
+for (int i=0; i< matrix.GetLength(0); i++)
+{
+    for (int j=0; j<matrix.GetLength(1); j++)
+    {
+        matrix [i, j]= new Random().NextDouble()+ new Random().Next(-10, 10);
+        matrix [i, j]= Math.Round(matrix[i, j], 2);
+        Console.Write(matrix[i,j] +" ");
+    }
+    Console.WriteLine();
+}
+
