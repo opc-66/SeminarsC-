c44277da326b928aa4fd866f7694606643307f77
sergey.osv <opc-66@yandex.ru>
2022-12-05 11:45:04 +0300

    Sem 8_HW2

--- Seminar_8/Sem8_HW/Sem8_HW2/Program.cs
+++ Seminar_8/Sem8_HW/Sem8_HW2/Program.cs
@@ -8,3 +8,53 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и 
 // выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.WriteLine("Введите кол-во строк");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите кол-во колонок");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int [,] array = new int [n,m];
+int [,] GetArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(0,10);
+        }
+    }
+    return array;
+}
+int [,] PrintArray(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]);
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+PrintArray(GetArray());
+
+int sum = 0;
+int minsum = Int32.MaxValue;
+int minsumrow =0;
+for (int i =0; i< array.GetLength(0); i++)
+{
+    for (int j=0; j<array.GetLength(1); j++)
+    {
+        sum=sum+array[i,j];
+    }
+    if (sum < minsum)
+    {
+        minsum= sum;
+        minsumrow = i;
+    }
+    sum = 0;
+}
+Console.WriteLine("Минимальная сумма " + minsum + " Индекс строки " +minsumrow);
