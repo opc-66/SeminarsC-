fbde1d1637fd9dbcd2d795090bd91be172efa72b
sergey.osv <opc-66@yandex.ru>
2022-12-06 23:34:24 +0300

    Sem 9_HW3

--- Seminar_9/Sem9_HW/Sem9_HW3/Program.cs
+++ Seminar_9/Sem9_HW/Sem9_HW3/Program.cs
@@ -3,4 +3,20 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+Console.WriteLine("Введите начальное число");
+int m = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Введите конечное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+int A(int m, int n)
+{
+     if (m==0) return n+1;
+     if (m !=0 && n==0) return A(m-1,1);
+     if (m>0 && n>0) return A(m-1, A(m, n-1));
+    return A(m,n);
+}
+
+Console.WriteLine(A(m,n)); 
