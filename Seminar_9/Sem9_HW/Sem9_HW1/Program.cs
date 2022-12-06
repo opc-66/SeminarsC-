80a9e161a53af4c980597460ccea925f68d44e78
sergey.osv <opc-66@yandex.ru>
2022-12-06 19:13:03 +0300

    Sem 9_HW1

--- Seminar_9/Sem9_HW/Sem9_HW1/Program.cs
+++ Seminar_9/Sem9_HW/Sem9_HW1/Program.cs
@@ -5,3 +5,16 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNum(n,1));
+
+string PrintNum(int start, int end)
+{
+    if (start==end)
+    {
+        return end.ToString();
+    }
+    return(start+ " "+PrintNum(start -1,  end));
+}    
