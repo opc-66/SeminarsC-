700f68742965532c8f594ab16c80ada92563a555
sergey.osv <opc-66@yandex.ru>
2022-12-06 22:10:24 +0300

    Sem 9_HW2

--- Seminar_9/Sem9_HW/Sem9_HW2/Program.cs
+++ Seminar_9/Sem9_HW/Sem9_HW2/Program.cs
@@ -5,3 +5,20 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.WriteLine("Введите начальное число");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите конечное число");
+int n = Convert.ToInt32(Console.ReadLine());
+//int sum= 0;
+Console.WriteLine(SumNum(m,n));
+
+int SumNum(int m, int n)
+{
+    if (m==n)
+    {
+        return m;
+    }
+    return(m+SumNum(m+1,n));
+
+}
